7152f4400f74635b8894a079dd61a1d884859937
najahiiii <tlogitechnjhi@gmail.com>
2019-03-01 20:40:58 +0700

    aarch64-linux-gnu-4.x-gnu-20190301

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+$ git clone https://github.com/najahiiii/aarch64-linux-gnu.git -b gcc4.9.4-20190301
+```
---
+++ lib/gcc/aarch64-linux-gnu/4.9.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --enable-languages=c,c++ --target=aarch64-linux-gnu --prefix=/home/najahi/cok/aarch64-linux-gnu";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
